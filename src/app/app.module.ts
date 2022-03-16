5b0c64ac485d8e52b170b328bf8843d6fdb76685
yaga49 <komarov.vova2001@mail.ru>
2022-03-16 19:56:16 +0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { PostbelowComponent } from './wrapper/postbelow/postbelow.component';
+import { ContentComponent } from './wrapper/content/content.component';
+import { HeaderComponent } from './wrapper/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    HeaderComponent,
+    WrapperComponent,
+    PostbelowComponent,
+
   ],
   imports: [
     BrowserModule,
