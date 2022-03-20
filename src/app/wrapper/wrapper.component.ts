aeb3e222edcb5c9bbf13e1c739a2425a23949422
yaga49 <komarov.vova2001@mail.ru>
2022-03-20 21:16:34 +0400

    Initial commit

--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {PostbelowComponent} from "./postbelow/postbelow.component";
 
 @Component({
   selector: 'app-wrapper',
   templateUrl: './wrapper.component.html',
   styleUrls: ['./wrapper.component.css']
 })
-export class WrapperComponent implements OnInit {
+export class WrapperComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
